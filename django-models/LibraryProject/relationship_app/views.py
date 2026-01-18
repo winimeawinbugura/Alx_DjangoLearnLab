23f2f9f4a1cde365a545063985a1ff29cf86b0d4
Winime Awinbugur Azumah <winimeawinbugura@gmail.com>
2026-01-18 22:51:57 +0000

    Django and more

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
+
+
+# Function-based view: List all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+# Class-based view: Display library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
